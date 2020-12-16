c2f87f1bf7da25beec3cd622eb3b6b6bf0e2c955
github_username <olaoluwaolaleye@gmail.com>
2020-12-16 02:06:09 -0800

    adding project

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+
+{
+	id: 1,
+	name: 'David Olaleye',
+	username: 'Dave',
+	email: 'BabyboyJeff@gmail.com'
+},
+
+{
+	id: 2,
+	name: 'Tofunmi Omojola',
+	username: 'Uwi',
+	email: 'Alakori@swagoon.com'
+},
+
+{
+	id: 3,
+	name: 'Temilola Olaleye',
+	username: 'Fufu',
+	email: 'Fufugang@egusi.com'
+},
+
+{
+	id: 4,
+	name: 'Michael Olaleye',
+	username: 'Miko',
+	email: 'Mikolala@usnavy.com'
+},
+
+{
+	id: 5,
+	name: 'Ed-Glory Olaleye',
+	username: 'Boli',
+	email: 'Boli@target.com'
+},
+
+{
+	id: 6,
+	name: 'James Olaleye',
+	username: 'paj',
+	email: 'MrJames@gmail.com'
+},
+
+{
+	id: 7,
+	name: 'Tomilola Olaleye',
+	username: 'Mummynson',
+	email: 'Mummy@texas.co.uk'
+
+},
+
+{
+	id: 8,
+	name: 'Feyisayo Idowu',
+	username: 'Feyo',
+	email: 'feyo@feyi.com'
+
+},
+
+{
+	id: 9,
+	name: 'Ajisafe Amos',
+	username: 'sorosoke',
+	email: 'Amos@abel.com'
+},
+
+{
+	id: 10,
+	name: 'Elijah Beechroft',
+	username: 'Eli',
+	email: 'Eli@enoch.com'
+}
+]
